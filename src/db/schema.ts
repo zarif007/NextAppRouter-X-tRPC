2ebdce058e73b0350dc91a33bda4704b179b1b2a
zarif007 <zarifhuq786@gmail.com>
2023-09-15 02:54:35 +0600

    [ADDED]: schema, drizzle, sqlite

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+
+export const todos = sqliteTable("todos", {
+    id: integer("id").primaryKey(),
+    content: text("content"),
+    completed: integer("completed")
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,23 @@
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import { publicProcedure, router } from "./trpc";
+import Database from 'better-sqlite3'
+
+import { todos } from '@/db/schema'
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { z } from "zod";
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite)
+
+migrate(db, { migrationsFolder: "drizzle" })
 
 export const appRouter = router({
     getTodos: publicProcedure.query(async () => {
-        return [10, 20, 30, 40];
+        return await db.select().from(todos).all()
+    }),
+    addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+        await db.insert(todos).values({ content: opts.input, completed: 0 }).run();
+        return true
     })
 });
 
